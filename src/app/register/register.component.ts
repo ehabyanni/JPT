a5726dca9d83284f50da6efaab9e8a8f8f05161e
ehabyanni <ehabayanni@gmail.com>
2025-05-02 01:55:54 +0300

    Full Responsive and controlled Register Form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -23,16 +23,32 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = this.formbuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
+      address: [''],
+      phone_number: ['', [Validators.pattern(/^[0-9]{10}$/)]],
+      birthdate: [''],
     });
   }
 
+  get NAME() {
+    return this.registerForm.get('name');
+  }
   get EMAIL() {
     return this.registerForm.get('email');
   }
   get PASSWORD() {
     return this.registerForm.get('password');
+  }
+  get ADDRESS() {
+    return this.registerForm.get('Address');
+  }
+  get PHONE() {
+    return this.registerForm.get('phone_number');
+  }
+  get BIRTHDATE() {
+    return this.registerForm.get('birthdate');
   }
 
   onSubmit() {
